7604d57fbdc12d3094b322055095ef10e233b533
Kia Raad <kia.panahirad@gmail.com>
2024-02-17 23:23:46 +0330

    Add ping instrument options type

---
+++ src/core/Monitoring/Ping/PingInstrumentOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Core.Monitoring.Ping;
+internal sealed class PingInstrumentOptions
+{
+    public PingInstrumentOptions(IPAddress ipAddress)
+    {
+        IPAddress = ipAddress;
+    }
+
+    public IPAddress IPAddress { get; }
+}
