545cb1467ba60618a5051b4b94d9e756fd77263f
Kia Raad <kia.panahirad@gmail.com>
2024-02-17 23:24:29 +0330

    Add cancelation token and ping instrument options to ping instrument

--- src/core/Monitoring/Ping/PingInstrument.cs
+++ src/core/Monitoring/Ping/PingInstrument.cs
@@ -4,12 +4,17 @@ namespace Core.Monitoring.Ping;
 internal sealed class PingInstrument : IMonitoringInstrument, IDisposable
 {
     private readonly SystemPing _pingSender;
+    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly PingInstrumentOptions _options;
     private bool _disposedValue;
 
-    public PingInstrument()
+    public PingInstrument(PingInstrumentOptions options, CancellationToken cancellationToken)
     {
         _pingSender = new SystemPing();
         _pingSender.PingCompleted += PingSender_PingCompleted;
+
+        _cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        _options = options;
     }
 
     private void PingSender_PingCompleted(object sender, System.Net.NetworkInformation.PingCompletedEventArgs e)
@@ -24,11 +29,13 @@ internal sealed class PingInstrument : IMonitoringInstrument, IDisposable
 
     private void Dispose(bool disposing)
     {
+        _cancellationTokenSource?.Cancel();
         if (!_disposedValue)
         {
             if (disposing)
             {
                 _pingSender?.Dispose();
+                _cancellationTokenSource?.Dispose();
             }
 
             // TODO: free unmanaged resources (unmanaged objects) and override finalizer
