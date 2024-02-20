3fe907f62eb6fe0f2008df513aae42d7c7ba0568
Kia Rad <kia.panahirad@gmail.com>
2024-02-20 18:09:13 +0330

    Dirty add usage of ping instrument in a live table

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,5 +1,6 @@
-﻿
-using Spectre.Console;
+﻿using System.Net;
+
+using Hyperion.Core.Monitoring.Ping;
 
 var t = new FigletText("Hyperion")
 {
@@ -7,3 +8,30 @@ var t = new FigletText("Hyperion")
     Color = Color.Cyan3
 };
 AnsiConsole.Write(t);
+
+var table = new Table()
+{
+    Border = TableBorder.DoubleEdge,
+};
+table.Centered()
+    .AddColumn("Ping", c => c.Centered());
+
+using var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += Console_CancelKeyPress;
+
+void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+{
+    cts.Cancel();
+}
+
+using var pinger = new PingInstrument(new PingInstrumentOptions(IPAddress.Parse("8.8.8.8")), cts.Token);
+await AnsiConsole.Live(table)
+    .StartAsync(async ctx =>
+    {
+        await foreach (var item in pinger.Start())
+        {
+            table.AddRow(new Markup($"{item.Duration}"));
+            ctx.Refresh();
+        }
+    });
