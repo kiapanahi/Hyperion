796e97b039332754daaacf06cc3e3f63f73cf006
Kia Raad <kia.panahirad@gmail.com>
2024-02-22 19:17:17 +0330

    Add base abstract class for all Monitoring instruments that implements dispose pattern and exposes a protected abstract DisposeCore method for inheritors to override

---
+++ src/core/Monitoring/MonitoringInstrumentBase.cs
@@ -0,0 +1,28 @@
+﻿namespace Hyperion.Core.Monitoring;
+
+public abstract class MonitoringInstrumentBase : IMonitoringInstrument
+{
+    private bool _disposedValue;
+
+    public abstract IAsyncEnumerable<ProbingResponse> Start();
+
+    protected abstract void DisposeCore();
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                DisposeCore();
+            }
+            _disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
