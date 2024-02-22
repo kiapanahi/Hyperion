dbc71cef3dcab0f1be6455376194290cddba9c4b
Kia Raad <kia.panahirad@gmail.com>
2024-02-22 23:27:07 +0330

    Add error formatting to HttpResponse type

--- src/core/Monitoring/Http/HttpResponse.cs
+++ src/core/Monitoring/Http/HttpResponse.cs
@@ -5,21 +5,30 @@ namespace Hyperion.Core.Monitoring.Http;
 public sealed class HttpResponse : ProbingResponse
 {
     private static readonly CompositeFormat ReportFormat = CompositeFormat.Parse("Reply: {0} [{1}] time={2}");
+    private static readonly CompositeFormat ErrorFormat = CompositeFormat.Parse("Reply: {0} Error: {1}");
 
     public HttpResponse(TimeSpan duration, Uri target, string statusCode) : base(duration)
     {
         Target = target;
         StatusCode = statusCode;
+        Error = null;
     }
 
-    public Uri Target { get; }
+    public HttpResponse(TimeSpan duration, string error) : base(duration)
+    {
+        Error = error;
+        StatusCode = string.Empty;
+    }
+
+    public Uri? Target { get; }
     public string StatusCode { get; }
+    public string? Error { get; }
 
     public override string Report()
     {
-        return string.Format(CultureInfo.InvariantCulture, ReportFormat,
-            Target,
-            StatusCode,
-            Duration.ToString(@"ss\.fffff", CultureInfo.InvariantCulture));
+        return !string.IsNullOrEmpty(Error)
+            ? string.Format(CultureInfo.InvariantCulture, ErrorFormat, Target, Error)
+            : string.Format(CultureInfo.InvariantCulture, ReportFormat, Target, StatusCode,
+                            Duration.ToString(@"ss\.fffff", CultureInfo.InvariantCulture));
     }
 }
