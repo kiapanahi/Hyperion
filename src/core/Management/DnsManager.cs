ca22958fbedd44e866fd62b50e4324b595f85608
Kia Rad <kia.panahirad@gmail.com>
2024-02-24 14:17:20 +0330

    Add DNS manager to flush DNS cache and its test

---
+++ src/core/Management/DnsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Hyperion.Core.Management;
+public sealed partial class DnsManager
+{
+    [LibraryImport("dnsapi.dll", EntryPoint = "DnsFlushResolverCache")]
+#pragma warning disable CA5392 // Use DefaultDllImportSearchPaths attribute for P/Invokes
+    private static partial uint FlushDns();
+#pragma warning restore CA5392 // Use DefaultDllImportSearchPaths attribute for P/Invokes
+
+
+#pragma warning disable CA1822 // Mark members as static
+    public void FlushDnsCache()
+    {
+        _ = FlushDns();
+    }
+#pragma warning restore CA1822 // Mark members as static
+}
